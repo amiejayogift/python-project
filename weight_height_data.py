5adab647ff0a2bfff364637e531b39653cd03b2d
amiejayogift <160616664+amiejayogift@users.noreply.github.com>
2024-09-16 14:06:36 +0100

    added bmi python project

---
+++ weight_height_data.py
@@ -0,0 +1,8 @@
+data = {
+    "Chukwudi": {'weight': '75', 'height': '180'},
+    "Arinze": {'weight': '100', 'height': '165'},
+    "Shed Tech": {'weight': '65', 'height': '159'},
+    "Lazy Programmer": {'weight': '65', 'height': '162'},
+    "Emmanu": {'weight': '60', 'height': '165'},
+    "Gift": {'weight': '70', 'height': '175'}
+}
